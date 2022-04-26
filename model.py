487cefcda3583cef7b692b28b6e9d6f9c11db806
nicolas.loontjens <nicolas.loontjens@student.howest.be>
2022-04-26 20:57:54 +0200

    split files into files with graphs and file with just the model

--- model.py
+++ model.py
@@ -1,70 +1,22 @@
 import numpy as np
 import pandas as pd
-import seaborn as sns
-import matplotlib.pyplot as plt
-from sklearn.linear_model import LinearRegression
-from sklearn.model_selection import train_test_split
-from sklearn.model_selection import cross_val_score
-from sklearn.linear_model import Ridge
-from sklearn.linear_model import Lasso
-from sklearn.pipeline import make_pipeline
-from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import ElasticNet
-from sklearn.metrics import mean_squared_error
-from sklearn.model_selection import GridSearchCV
-##some configuration so that we can view everything in the console
-pd.set_option('display.max_rows', None)
-pd.set_option('display.max_columns', None)
-pd.set_option('display.width', None)
-pd.set_option('display.max_colwidth', -1)
-
-##importing the datasets
 
 training_dataset = pd.read_csv("./train.csv")
 test_dataset = pd.read_csv("./test.csv")
-data = training_dataset
-#pd.concat([training_dataset.drop('SalePrice',axis=1),test_dataset],keys=['train','test'])
-data.drop('Id',axis=1, inplace=True)
 
-"""
-first we will check the data and make sure it is correct:
-we should check the years, make sure that there aren't any sales that exceed the current year because that would be impossible
-we should check the prices and measurements to see if they are not negative
-"""
+test_id = test_dataset['Id']
+
+data = pd.concat([training_dataset.drop('SalePrice', axis=1), test_dataset], keys=['train', 'test'])
+data.drop(['Id'], axis=1, inplace=True)
 
 columns_with_years = ['YearBuilt','GarageYrBlt','YrSold','YearRemodAdd']
 columns_with_measurements = ['LotFrontage','LotArea','MasVnrArea','BsmtFinSF1','BsmtFinSF2','BsmtUnfSF','TotalBsmtSF','1stFlrSF','2ndFlrSF', 
                              'LowQualFinSF', 'GrLivArea', 'GarageArea', 'WoodDeckSF', 
                             'OpenPorchSF', 'EnclosedPorch', '3SsnPorch', 'ScreenPorch', 'PoolArea', 'MiscVal']
-
-##here we print the maximum years for a specific row and check if it exceeds 2022 => in this case we saw that the test dataset had a max of 2207 for GarageYrBlt
-print(data[columns_with_years].max()) 
-
-##here we check with an if statement if any columns with years in them exceed 2022, if so we grab the row
 wrong_years = (data[columns_with_years] > 2022).any(axis=1)
-print("rows with a wrong year:") 
-print(data[wrong_years])
-
-##in this situation we can see that the yearbuilt of the wrong garageyrblt entry is right so we replace garageyrblt with the yearblt value of the house
 data.loc[wrong_years,'GarageYrBlt'] = data[wrong_years]['YearBuilt'] 
 
-##now the years are all good 
-print(data[columns_with_years].max())
-
-##next up we will check the measurements
-wrong_measurements = (data[columns_with_measurements] < 0).any(axis=1)
-
-##this is empty, thus there are no wrong measurements
-print("rows with impossible measurements:")
-print(data[wrong_measurements])
-
-##lets see what our data looks like again
-print(data.shape)
-
-##next up we will split the features and modify the data
-# we want to split the features containing sqfeet, prices, etc
-# and the features with data containing "Excelllent, good, etc."
-# we will then convert the 2nd list of features to a numerical scale since this will be easier to use
 numerical_features = ['LotFrontage', 'LotArea', 'OverallQual', 'OverallCond', 
              'YearBuilt', 'YearRemodAdd', 'MasVnrArea', 'ExterQual', 'ExterCond', 'GarageQual', 'GarageCond',
              'BsmtQual', 'BsmtCond', 'BsmtFinSF1', 'BsmtFinSF2', 'BsmtUnfSF', 'TotalBsmtSF', 
@@ -76,15 +28,82 @@ numerical_features = ['LotFrontage', 'LotArea', 'OverallQual', 'OverallCond',
              'ScreenPorch', 'MiscVal','YrSold']
 
 grading_features = ['OverallQual','OverallCond','GarageCond','GarageQual','ExterQual','ExterCond','BsmtQual','BsmtCond','HeatingQC','KitchenQual','PoolQC','FireplaceQu']  
-
 text_grades = ['Ex','Gd','TA','Fa','Po']
 numerical_grading = [9,7,5,3,1]
 gradingdictionary = dict(zip(['Ex','Gd','TA','Fa','Po'],[9,7,5,3,1]))
-##here we replace the Ex, Gd with 9, 7, etc., now we don't have to use dummies for these columns
 data[grading_features] = data[grading_features].replace(gradingdictionary)
 
-##these are the features that contain categories of data
 categorical_features = data.drop(numerical_features,axis=1).columns
 
-print(categorical_features)
-##and now we will fit the data to the model, starting with LinearRegression
+features = ['MSZoning', 'Utilities', 'Exterior1st', 'Exterior2nd', 'Electrical', 'Functional',
+         'SaleType']
+def fun(x):
+    return x.mode().iloc[0]
+model = data.groupby('Neighborhood')[features].apply(fun)
+
+for f in features:
+    data[f].fillna(data['Neighborhood'].map(model[f]), inplace=True)
+
+data['LotFrontage'] = data['LotFrontage'].fillna(data['LotFrontage'].median())
+data['KitchenQual'].fillna(data['OverallQual'], inplace=True)
+
+
+category = data.columns[data.dtypes == 'object']
+numerical = list(set(data.columns)-set(category))
+
+data['MasVnrType'] = data['MasVnrType'].replace({'None':np.nan})
+
+data[category] = data[category].fillna('0')
+data[numerical] = data[numerical].fillna(0)
+
+data['MoSold'] = data['MoSold'].astype('object', copy=False)
+data['BsmtHalfBath'] = data['BsmtHalfBath'].astype('int64', copy=False)
+data['BsmtFullBath'] = data['BsmtFullBath'].astype('int64', copy=False)
+data['MSSubClass'] = data['MSSubClass'].astype('object', copy=False)
+data['GarageCars'] = data['GarageCars'].astype('int64', copy=False)
+
+categorical_data = data[categorical_features]
+
+for feature in categorical_features:
+    unique_values = pd.unique(data[feature])
+    if(len(unique_values) < 2):
+        data.drop(feature,axis=1,inplace=True)
+    elif(len(unique_values) < 3):
+        data[feature].replace({unique_values[0]:0,unique_values[1]:1},inplace=True)
+        data[feature] = data[feature].astype('int8')
+    else:
+        data[feature] = data[feature].astype('category')
+
+finaldataformodel = pd.get_dummies(data)
+
+feat_list = ['BsmtQual', 'BsmtCond', 'BsmtExposure', 'BsmtFinType1', 
+        'BsmtFinSF1', 'BsmtFinType2', 'BsmtFinSF2', 'BsmtUnfSF', 'BsmtFullBath',
+        'BsmtHalfBath', 'TotalBsmtSF', 'GarageQual', 'GarageCond', 'GarageType', 'GarageFinish', 'GarageCars', 
+          'GarageArea', 'GarageYrBlt', 'Fireplaces', 'FireplaceQu','MasVnrType', 'MasVnrArea','Alley', 'Fence', 'PoolQC', 'MiscFeature']
+for feature in finaldataformodel.columns:
+    if('_0' in feature) and (feature.split('_')[0] in feat_list):
+        finaldataformodel.drop(feature,axis=1,inplace=True)
+
+
+test = finaldataformodel.loc['test']
+train = finaldataformodel.loc['train']
+
+X = train
+y = training_dataset['SalePrice']
+
+elastic_net = ElasticNet(l1_ratio=0.9655172413793103)
+
+elastic_net.fit(X,y)
+
+submission = elastic_net.predict(test)
+result = pd.DataFrame()
+result['Id'] = test_dataset['Id']
+result['SalePrice'] = submission
+
+result.to_csv('submission.csv',index=False)
+
+
+
+
+
+
